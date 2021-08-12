1de19a5adeb901664e92f916865a5bee29edb281
Eli Tokar <52536795+elitGithub@users.noreply.github.com>
2021-08-13 01:21:38 +0300

    more funky db stuff

--- backend/Migrations/m010_MakeSoundex.php
+++ backend/Migrations/m010_MakeSoundex.php
@@ -25,6 +25,11 @@ class m010_MakeSoundex extends Migration
 
     public function down()
     {
-        // TODO: Implement down() method.
+        foreach ($this->tables as $table => $column) {
+            $result = $this->db->preparedQuery("SELECT $column FROM $table");
+            foreach ($this->db->fetchAllColumns($result) as $string) {
+                $this->db->preparedQuery("UPDATE $table SET sounds_like = NULL WHERE $column = BINARY ?", [$string]);
+            }
+        }
     }
 }
